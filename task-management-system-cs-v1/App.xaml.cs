50b9d3372159721fbcd85e8df5490886879b21de
YukiChriss <naoyuki.christopher.higaki@outlook.com>
2025-06-28 22:25:12 +0200

    Add project files.

---
+++ task-management-system-cs-v1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace task_management_system_cs_v1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
